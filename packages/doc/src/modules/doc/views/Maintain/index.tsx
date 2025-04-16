bcd17b7a9dd5ae26c101514040368b24d4ed06d2
qiaozhaomin <zhaomin.qiao@17gobig.com>
2025-04-16 18:08:21 +0800

    增加easyEdit组件调用示例

--- packages/doc/src/modules/doc/views/Maintain/index.tsx
+++ packages/doc/src/modules/doc/views/Maintain/index.tsx
@@ -2,12 +2,12 @@ import {DeleteOutlined, DownOutlined, ExceptionOutlined, FolderAddOutlined, Plus
 import {DocumentHead, Link} from '@elux/react-web';
 import {Breadcrumb, Button, Dropdown, Form, Input, Popover, Space, Table, TableProps, Upload} from 'antd';
 import {FC, memo, useEffect, useMemo, useState} from 'react';
+import EasyEdit from '@/components/EasyEdit';
 import {GetClientRouter} from '@/Global';
 import {confirm, getUrlParam, message, useEvent, useSingleWindow} from '@/utils/tools';
 import {DocAPI} from '../../api';
 import {ListItem, ListSearch, ListSummary} from '../../entity';
 import styles from './index.module.less';
-
 interface Props {
   listSearch: ListSearch;
   list: ListItem[];
@@ -191,6 +191,54 @@ const Component: FC<Props> = ({list, listSearch, listSummary}) => {
   return (
     <div className={'g-page-content ' + styles.root}>
       <DocumentHead title="我的文档" />
+      {/* <EasyEdit
+        tpl="你是一名${role}，需要整理本周工作周报，本周主要工作内容为${text}，下周主要工作计划为${newText}"
+        option={{
+          role: [
+            {label: '管理员', key: '管理员'},
+            {label: '普通职员', key: '普通职员'},
+          ],
+          text: [
+            {label: '改了两个bug', key: '改了两个bug'},
+            {label: '做了一个新需求', key: '做了一个新需求'},
+          ],
+        }}
+        value={{
+          text: '你是一名管理员，需要整理本周工作周报，本周主要工作内容为 ，下周主要工作计划为 ',
+          tplValue: [
+            {
+              key: '你是一名',
+              type: 'text',
+              value: '你是一名',
+            },
+            {
+              key: 'role',
+              type: 'variable',
+              value: '管理员',
+            },
+            {
+              key: '，需要整理本周工作周报，本周主要工作内容为',
+              type: 'text',
+              value: '，需要整理本周工作周报，本周主要工作内容为',
+            },
+            {
+              key: 'text',
+              type: 'variable',
+              value: ' ',
+            },
+            {
+              key: '，下周主要工作计划为',
+              type: 'text',
+              value: '，下周主要工作计划为',
+            },
+            {
+              key: 'newText',
+              type: 'variable',
+              value: ' ',
+            },
+          ],
+        }}
+      /> */}
       <div className="hd">{breadcrumb}</div>
       <div className="cd">
         <Space>
