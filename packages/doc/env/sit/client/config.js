0a6ee1950207f4b2ad27178a1f3824654166cc61
hiisea <hiisea@qq.com>
2025-08-18 18:41:33 +0800

    体会用户中心地址

--- packages/doc/env/sit/client/config.js
+++ packages/doc/env/sit/client/config.js
@@ -1,6 +1,6 @@
 window.ApiBaseUrl = '';
 window.ApiPrefix = {
-  '/auth/': 'http://8.130.27.129/auth/',
+  '/auth/': 'http://user.binarysee.com.cn/auth/',
   '/dream/': 'http://113.44.121.105/dream/',
 };
 window.SitesUrl = {
