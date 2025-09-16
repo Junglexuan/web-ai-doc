9b848cd804865314dbb4b7b8600d7560bbffeeed
hiisea <hiisea@qq.com>
2025-09-16 14:39:20 +0800

    调整用户切换

--- packages/doc/src/modules/admin/views/UserCenterMenu/index.tsx
+++ packages/doc/src/modules/admin/views/UserCenterMenu/index.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useRef} from 'react';
 import {SitesUrl} from '@/Global';
-import {getTenant, getToken} from '@/utils/tools';
+import {getTenant, getToken, info} from '@/utils/tools';
 
 const UserMenu: React.FC<{username: string; logout: () => void; switchTenant: () => void}> = ({username, logout, switchTenant}) => {
   const userMenuRef: any = useRef<HTMLElement | null>(null);
@@ -8,13 +8,24 @@ const UserMenu: React.FC<{username: string; logout: () => void; switchTenant: ()
   useEffect(() => {
     const el = userMenuRef.current;
     if (!el) return;
+
+    const errorCallback = (data: CustomEvent) => {
+      const res = data.detail[0];
+      if (res.status === 402) {
+        info(res.data.message || '检测到租户已发生变化，需要刷新数据...', () => {
+          switchTenant();
+        });
+      }
+    };
     //通过原生自定义事件方式订阅事件
     userMenuRef.current.addEventListener('logout', logout);
     userMenuRef.current.addEventListener('switch-tenant', switchTenant);
+    userMenuRef.current.addEventListener('request-error', errorCallback);
 
     return () => {
       userMenuRef.current.removeEventListener('logout', logout);
       userMenuRef.current.removeEventListener('switch-tenant', switchTenant);
+      userMenuRef.current.removeEventListener('request-error', errorCallback);
     };
   }, [logout]);
 
